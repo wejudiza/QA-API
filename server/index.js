8cda8155e4049119c6a5ca115688fc033ce7fe50
weillytong <weillydtong@gmail.com>
2021-03-16 15:23:49 -0700

    Set up package.json and server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+// const router = require('./router.js');
+
+const port = 8080;
+
+const server = express();
+
+// Apply Middleware
+server.use(morgan('dev'));
+server.use(express.json());
+server.use(cors());
+server.use(bodyparser.json());
+server.use(bodyparser.urlencoded({ extended: true }));
+
+// server.use('/api', router);
+
+server.listen(port, () => console.log(`LISTENING ON PORT ${port}`));
---
+++ server/router.js
@@ -0,0 +1,32 @@
+// const router = require('express').Router();
+// const controller = require('./controller');
+
+// /// ///////Questions and Answers///////////
+
+// router
+//   .route('/qa/questions/:product_id')
+//   .get(controller.getQnA)
+//   .post(controller.postQuestion);
+
+// router
+//   .route('/qa/questions/:question_id/answers')
+//   .get(controller.getAnswers)
+//   .post(controller.postAnswer);
+
+// router
+//   .route('/qa/answers/:answer_id/report')
+//   .put(controller.reportAnswer)
+
+// router
+//   .route('/qa/answers/:answer_id/helpful')
+//   .put(controller.voteHelpful)
+
+// router
+//   .route('/qa/questions/:question_id/helpful')
+//   .put(controller.voteQuestionHelpful)
+
+// router
+//   .route('/qa/questions/:question_id/report')
+//   .put(controller.reportQuestion)
+
+// module.exports = router;
